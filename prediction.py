dd69a47dd0e7a979fad9ca5037aa6484262e9b82
Audrey Reznik <adrezni@redhat.com>
2021-06-30 22:46:01 +0000

    add new files and add prediction function

---
+++ prediction.py
@@ -0,0 +1,17 @@
+# For sample predict functions for popular libraries visit https://github.com/opendatahub-io/odh-prediction-samples
+
+# Import libraries
+# import tensorflow as tf
+
+# Load your model.
+# model_dir = 'models/myfancymodel'
+# saved_model = tf.saved_model.load(model_dir)
+# predictor = saved_model.signatures['default']
+
+
+# Write a predict function 
+def predict(args_dict):
+#     arg = args_dict.get('arg1')
+#     predictor(arg)
+    return {'prediction': 'not implemented'}
+
---
+++ wsgi.py
@@ -0,0 +1,17 @@
+import json
+from flask import Flask, jsonify, request
+from prediction import predict
+
+application = Flask(__name__)
+
+@application.route('/')
+@application.route('/status')
+def status():
+    return jsonify({'status': 'ok'})
+
+
+@application.route('/prediction', methods=['POST'])
+def object_detection():
+    data = request.data or '{}'
+    body = json.loads(data)
+    return jsonify(predict(body))
